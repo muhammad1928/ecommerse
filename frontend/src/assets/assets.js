2d127ffae32160f46ace1504f193324bf45946b7
muhammad1928 <muhammadsn7@gmail.com>
2024-11-18 00:27:00 +0100

    start1

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,56 @@
+// defining images
+import cicek1 from './cicek1'
+import cicek2 from './cicek2'
+import cicek3 from './cicek3'
+import cicek4 from './cicek4'
+import cicek5 from './cicek5'
+import cicek6 from './cicek6'
+import cicek7 from './cicek7'
+import cicek8 from './cicek8'
+import cicek9 from './cicek9'
+import cicek10 from './cicek10'
+
+// exporting the defined images
+export const assets = {
+    cicek1,
+    cicek2,
+    cicek3,
+    cicek4,
+    cicek5,
+    cicek6,
+    cicek7,
+    cicek8,
+    cicek9,
+    cicek10,
+}
+
+
+
+export const produc= [
+    {
+        _id : "cicek1",
+        name: "kirmizi gul",
+        description: "Kirmizi 6 adet gul",
+        price: 100,
+        image: [cicek1],
+        category: "cicek",
+        subCategory : "sevgili-Cicek",
+        sizes : [5,10, 15, 20],
+        date: 123123123,
+        bestSeller : true
+
+    },
+    {
+        _id : "cicek1",
+        name: "kirmizi gul",
+        description: "Kirmizi 6 adet gul",
+        price: 100,
+        image: [cicek1],
+        category: "cicek",
+        subCategory : "sevgili-Cicek",
+        sizes : [5,10, 15, 20],
+        date: 123123123,
+        bestSeller : true
+
+    }
+]
---
+++ readme.md
@@ -0,0 +1,24 @@
+
+npm create vite@latest
+
+create-vite@5.5.5
+```bash
+project name = frontend
+cd frontend
+npm install
+npm install react-router-dom react-toastify
+```
+
+``` react-router-dom ``` we can create multiple doms using this
+
+``` react-toastify ``` sending toast notifications
+
+to run project type ``` npm run dev``` inside frontend folder
+
+delete App.css file in src folder. also remove all the code in app.jsx file and type
+
+``rafce`` and enter
+
+delete react.svg file inside src/assets
+
+clear the index.css code
